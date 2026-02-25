f87e7569328d5c3642eaabe8e275a922a59ced49
mehmet ata suyabatmaz 35491 <atasuyabatmaz@gmail.com>
2026-02-25 08:28:43 +0000

    main.py folder and name-surname

---
+++ students/35491-MehmetAtaSuyabatmaz/main.py
@@ -0,0 +1,26 @@
+# task 1. write your name on the screen
+print("MehmetAtaSuyabatmaz")
+
+
+# variable for number
+
+
+
+# variable for text (string)
+
+
+
+# check the type of the variable
+
+
+# create a list of numbers
+
+
+
+# print(list1[0])
+
+for i in list1:
+    print(i)
+
+
+
